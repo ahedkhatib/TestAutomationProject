4c570b7af4c99b184628bc861db45ff030bc402b
ahedkhatib <Ahedkhatib.2001@gmail.com>
2025-01-17 17:50:37 +0200

    test

--- src/main/java/org/example/LoginPage.java
+++ src/main/java/org/example/LoginPage.java
@@ -31,21 +31,21 @@ public class LoginPage {
 
     public HomePage loginAs(String username, String password) {
         System.out.println("Navigating to login page...");
-        wait.until(ExpectedConditions.urlContains("/login"));
+        System.out.println("Current URL: " + driver.getCurrentUrl());
+
+        wait.until(ExpectedConditions.urlToBe("https://5f69-2a06-c701-7116-c00-1677-13ff-c635-10a9.ngrok-free.app"));
+        System.out.println("Reached the login page.");
 
         System.out.println("Waiting for username field...");
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("username")));
-        usernameField.sendKeys(username);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("username"))).sendKeys(username);
         System.out.println("Username entered.");
 
         System.out.println("Waiting for password field...");
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("password")));
-        passwordField.sendKeys(password);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("password"))).sendKeys(password);
         System.out.println("Password entered.");
 
         System.out.println("Waiting for login button...");
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("button[type='submit']")));
-        loginButton.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='submit']"))).click();
         System.out.println("Login button clicked.");
 
         return new HomePage(driver);
