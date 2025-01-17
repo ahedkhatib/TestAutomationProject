55477f1f09b2f3b1662d8e20cbd188bf699cbd84
ahedkhatib <Ahedkhatib.2001@gmail.com>
2025-01-17 21:05:52 +0200

    test

--- src/test/java/selenium/EditMetadataTest.java
+++ src/test/java/selenium/EditMetadataTest.java
@@ -143,7 +143,7 @@ public class EditMetadataTest {
         assertEquals(3, actualRating);
     }
 
-    /*@Test
+    @Test
     public void testClearRating() {
         String successMessage = new LoginPage(driver)
                 .loginAs("admin", "admin123")
@@ -152,9 +152,9 @@ public class EditMetadataTest {
                 .clearRating();
 
         assertEquals("Metadata successfully updated", successMessage);
-    }*/
+    }
 
-    /*@Test
+    @Test
     public void testEditPublishedDate() {
         String successMessage = new LoginPage(driver)
                 .loginAs("admin", "admin123")
@@ -193,7 +193,7 @@ public class EditMetadataTest {
 
         assertEquals("Metadata successfully updated", successMessage);
         assertEquals("Language: English", actualLanguage);
-    }*/
+    }
 
     @AfterEach
     public void tearDown() {
